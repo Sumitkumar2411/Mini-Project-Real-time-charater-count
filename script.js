ff4aea1c47d4fd432e36355a102d8eea380770d8
Sumitkumar2411 <sumitnandan2025@gmail.com>
2025-08-02 20:24:53 +0530

    real-time-charCount

---
+++ script.js
@@ -0,0 +1,28 @@
+const textarea = document.getElementById("message");
+const charCount = document.getElementById("char-count");
+const wordCount = document.getElementById("word-count");
+const submitBtn = document.getElementById("submit-btn");
+
+const maxLength = 100;
+
+textarea.addEventListener("input", () => {
+  const text = textarea.value;
+
+  // Character count
+  const currentLength = text.length;
+  charCount.textContent = `Characters: ${currentLength} / ${maxLength}`;
+
+  // Optional warning color
+  charCount.style.color = currentLength > 80 ? "red" : "black";
+
+  //  Word count (ignore extra spaces)
+  const words = text.trim().split(/\s+/).filter(word => word.length > 0);
+  wordCount.textContent = `Words: ${words.length}`;
+
+  // Button enable/disable
+  if (currentLength === 0 || currentLength > maxLength) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
+  }
+});
